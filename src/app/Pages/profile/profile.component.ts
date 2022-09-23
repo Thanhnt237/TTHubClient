0819b7f9c7e5e5d773b0f73dc03d96c5a00dc20d
ThanhNt <admater99@gmail.com>
2022-09-23 20:06:07 +0700

    .

--- src/app/Pages/profile/profile.component.ts
+++ src/app/Pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +12,19 @@ export class ProfileComponent implements OnInit {
   breakpoint: any = null;
 
   constructor(
+      private _formBuilder: FormBuilder,
+      private _snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     this.breakpoint = (window.innerWidth <= 400) ? 1 : 6
   }
 
-  onResize(event: any) {
-    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
+  createPostForm: FormGroup = this._formBuilder.group({
+    postContent : new FormControl('', [Validators.required, Validators.email])
+  });
+
+  onSubmitForm(){
+
   }
 }
