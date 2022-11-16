1fc113a22f1ec444069bd1913bcdaf0396f23a21
ThanhNt <admater99@gmail.com>
2022-11-16 17:15:26 +0700

    fack today

--- src/app/Pages/study-management/add-study-dialog/add-study-dialog.component.ts
+++ src/app/Pages/study-management/add-study-dialog/add-study-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
+import { KloudNotificationService } from "../../../Components/kloud-notification/kloud-notification.service";
+import { ClassService } from "../../../Services/class/class.service";
 
 @Component({
   selector: 'add-study-dialog',
@@ -7,7 +12,65 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AddStudyDialogComponent implements OnInit{
 
+  apiLoading: boolean = false;
+  isNew: boolean = false
+
+  constructor(
+    private dialogRef: MatDialogRef<AddStudyDialogComponent>,
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _kloudNoti: KloudNotificationService,
+    private classService: ClassService,
+    @Inject(MAT_DIALOG_DATA) public classRecordData: any
+  ) {
+  }
+
+  studyInformationForm: FormGroup = this._formBuilder.group({
+    month: new FormControl(),
+    user: new FormControl(),
+    student: new FormControl(),
+    educational_goals: new FormControl(),
+    student_goals_achieved: new FormControl(),
+    solution_orientation: new FormControl(),
+  })
+
+  userInfoForm: FormGroup = this._formBuilder.group({
+    ID: new FormControl(),
+    name: new FormControl(),
+  })
+
+  studentInfoForm: FormGroup = this._formBuilder.group({
+    ID: new FormControl(),
+    name: new FormControl()
+  })
+
+  educationGoalForm: FormGroup = this._formBuilder.group({
+    physical_education: new FormControl(),
+    intelligence_education: new FormControl(),
+    language_education: new FormControl(),
+    socialite_education: new FormControl(),
+    art_education: new FormControl(),
+    health: new FormControl(),
+    english_education: new FormControl(),
+    life_skill_bank: new FormControl(),
+  })
+
+  student_goals_achieved: FormGroup = this._formBuilder.group({
+    physical_education: new FormControl(),
+    intelligence_education: new FormControl(),
+    language_education: new FormControl(),
+    socialite_education: new FormControl(),
+    art_education: new FormControl(),
+    health: new FormControl(),
+    english_education: new FormControl(),
+    life_skill_bank: new FormControl(),
+  })
+
   ngOnInit(): void {
+
+  }
+
+  onOK(){
 
   }
 
--- src/app/Pages/study-management/study-management.component.ts
+++ src/app/Pages/study-management/study-management.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { StudentService } from "../../Services/students/student.service";
 import { KloudNotificationService } from "../../Components/kloud-notification/kloud-notification.service";
 import { ClassService } from "../../Services/class/class.service";
+import { AddStudyDialogComponent } from "./add-study-dialog/add-study-dialog.component";
 
 @Component({
   selector: 'app-study-management',
@@ -17,6 +18,9 @@ export class StudyManagementComponent implements OnInit {
 
   studentDataSource: any[] = [];
   selectedStudentModel: any;
+
+  semesterDataSource: any[] = [];
+  selectedSemesterModel: any;
 
   classesDataSource: any[] = [];
   selectedClassModel: any;
@@ -34,13 +38,21 @@ export class StudyManagementComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.handleGetClass()
+    this.handleGetSemester()
   }
 
   handleGetStudent(){
-    this._studentService.getStudentsInfo().subscribe(
+    this.apiLoading = true
+    const query = {
+      filter_semester: this.selectedSemesterModel,
+      filter_class: this.selectedClassModel
+    }
+
+    this._studentService.getStudentsInfo(query).subscribe(
       (res: any) => {
-        this.studentDataSource = res.data
+        console.log(res);
+        this.studentDataSource = res?.data
+        this.apiLoading = false
       }, error => {
         this._kloudNoti.error(error)
       }
@@ -48,17 +60,51 @@ export class StudyManagementComponent implements OnInit {
   }
 
   handleGetSemester(){
+    this.apiLoading = true
+    this._classService.getSemester().subscribe(
+      (res: any) => {
+        this.semesterDataSource = res
+        if(this.semesterDataSource?.length) this.selectedSemesterModel = this.semesterDataSource[0].semester
 
+        this.handleGetClass()
+        this.apiLoading = false
+      }, error => {
+        this._kloudNoti.error(error)
+      }
+    )
   }
 
   handleGetClass(){
-    this._classService.handleGetClasses().subscribe(
+    this.apiLoading = true
+    const query = {
+      semester: this.selectedSemesterModel
+    }
+    this._classService.handleGetClasses(query).subscribe(
       (res: any) => {
-        console.log(res);
+        this.classesDataSource = res?.data
+        this.apiLoading = false
       }, error => {
         this._kloudNoti.error()
       }
     )
+  }
+
+  onSelectedClass(){
+    this.handleGetStudent()
+  }
+
+  onSelectedSemester(){
+    this.handleGetClass()
+  }
+
+  openAddClassDialog(){
+    this._dialog.open(AddStudyDialogComponent, {
+      width: "1360px"
+    })
+      .afterClosed()
+      .subscribe(res => {
+        if(res === 'success') console.log(res);
+      })
   }
 
   handleSearch(){
