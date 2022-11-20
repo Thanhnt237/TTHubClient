3b0bb8af05a9b47857397a17bae0460f2813e721
ThanhNt <admater99@gmail.com>
2022-11-20 07:33:55 +0700

    fack today

--- src/app/Pages/study-management/add-study-dialog/add-study-dialog.component.ts
+++ src/app/Pages/study-management/add-study-dialog/add-study-dialog.component.ts
@@ -1,14 +1,42 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { KloudNotificationService } from "../../../Components/kloud-notification/kloud-notification.service";
 import { ClassService } from "../../../Services/class/class.service";
 
+import {Moment} from 'moment-timezone';
+import { MatDatepicker } from "@angular/material/datepicker";
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from "@angular/material/core";
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from "@angular/material-moment-adapter";
+
+const MONTH_YEAR_FORMAT = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 @Component({
   selector: 'add-study-dialog',
   templateUrl: 'add-study-dialog.component.html',
-  styleUrls: ['add-study-dialog.component.scss']
+  styleUrls: ['add-study-dialog.component.scss'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: MONTH_YEAR_FORMAT
+    },
+  ],
 })
 export class AddStudyDialogComponent implements OnInit{
 
@@ -29,9 +57,11 @@ export class AddStudyDialogComponent implements OnInit{
   }
 
   studyInformationForm: FormGroup = this._formBuilder.group({
-    month: new FormControl(),
-    user: new FormControl(),
-    student: new FormControl(),
+    month: new FormControl("",[Validators.required]),
+    user: new FormControl("",[Validators.required]),
+    student: new FormControl("",[Validators.required]),
+    class: new FormControl("",[Validators.required]),
+    guideline: new FormControl("", [Validators.required]),
     educational_goals: new FormControl(),
     student_goals_achieved: new FormControl(),
     solution_orientation: new FormControl(),
@@ -48,25 +78,25 @@ export class AddStudyDialogComponent implements OnInit{
   })
 
   educationGoalForm: FormGroup = this._formBuilder.group({
-    physical_education: new FormControl(),
-    intelligence_education: new FormControl(),
-    language_education: new FormControl(),
-    socialite_education: new FormControl(),
-    art_education: new FormControl(),
-    health: new FormControl(),
-    english_education: new FormControl(),
-    life_skill_bank: new FormControl(),
+    physical_education: new FormControl("",[Validators.required]),
+    intelligence_education: new FormControl("",[Validators.required]),
+    language_education: new FormControl("",[Validators.required]),
+    socialite_education: new FormControl("",[Validators.required]),
+    art_education: new FormControl("",[Validators.required]),
+    health: new FormControl("",[Validators.required]),
+    english_education: new FormControl("",[Validators.required]),
+    life_skill_bank: new FormControl("",[Validators.required]),
   })
 
    student_goals_achieved: FormGroup = this._formBuilder.group({
-    physical_education: new FormControl(),
-    intelligence_education: new FormControl(),
-    language_education: new FormControl(),
-    socialite_education: new FormControl(),
-    art_education: new FormControl(),
-    health: new FormControl(),
-    english_education: new FormControl(),
-    life_skill_bank: new FormControl(),
+    physical_education: new FormControl("",[Validators.required]),
+    intelligence_education: new FormControl("",[Validators.required]),
+    language_education: new FormControl("",[Validators.required]),
+    socialite_education: new FormControl("",[Validators.required]),
+    art_education: new FormControl("",[Validators.required]),
+    health: new FormControl("",[Validators.required]),
+    english_education: new FormControl("",[Validators.required]),
+    life_skill_bank: new FormControl("",[Validators.required]),
   })
 
   ngOnInit(): void {
--- src/app/Pages/study-management/study-management-table/study-management-table.component.ts
+++ src/app/Pages/study-management/study-management-table/study-management-table.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit } from "@angular/core";
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: ``, weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: "study-management-table",
   templateUrl: "study-management-table.component.html",
   styleUrls: ["study-management-table.component.scss"]
 })
-export class StudyManagementTableComponent implements OnInit{
+export class StudyManagementTableComponent implements OnInit, OnChanges{
+  @Input() studyInformationForm: FormGroup
+  @Input() educationGoalForm: FormGroup
+  @Input() student_goals_achieved: FormGroup
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  studyInformationData: any
+  educationGoalData: any
+  studentGoalsAchievedData: any
+
+  constructor(
+
+  ) {
+  }
 
   ngOnInit(): void {
 
   }
+
+  parseChanges(changes: any){
+    this.studyInformationData = changes["studyInformationForm"].currentValue
+    this.educationGoalData = changes["educationGoalForm"].currentValue
+    this.studentGoalsAchievedData = changes["student_goals_achieved"].currentValue
+
+    this.studyInformationData.valueChanges.subscribe((res: any) => {
+      this.studyInformationData = res
+    })
+
+    this.educationGoalData.valueChanges.subscribe((res: any) => {
+      this.educationGoalData = res
+      console.log(res);
+    })
+
+    this.studentGoalsAchievedData.valueChanges.subscribe((res: any) => {
+      this.studentGoalsAchievedData = res
+    })
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.parseChanges(changes)
+  }
+
 
 }
